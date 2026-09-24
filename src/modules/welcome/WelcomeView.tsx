5355cdeebba7199bc9b205f0340fb3bb59ba80d7
xmasuhai <xmasuhai@163.com>
2026-09-24 21:53:05 +0800

    style💄/refactor(welcome): 将scoped样式转换为module样式
    - 移除welcome.scoped.scss文件
    - 添加welcome.module.scss文件
    - 更新WelcomeView组件以使用CSS模块
    - 修改类名引用方式从class到class={s.className}

--- src/modules/welcome/WelcomeView.tsx
+++ src/modules/welcome/WelcomeView.tsx
@@ -1,12 +1,14 @@
 import { defineComponent } from 'vue'
-import '@/modules/welcome/welcome.scoped.scss'
 import { RouterView } from 'vue-router'
+import s from '@/modules/welcome/welcome.module.scss'
 
 export const WelcomeView = defineComponent({
   setup: (/* props, context */) => {
+    console.log('WelcomeView', s)
+
     return () => (
-      <div class="wrapper">
-        <header class="wrapper__tip">
+      <div class={s.wrapper}>
+        <header class={s.title}>
           <h1>山竹记账</h1>
         </header>
         <main>
