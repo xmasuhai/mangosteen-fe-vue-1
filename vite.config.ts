d3feab6b77735a338d361b06ccfb4eb3e48bf0b0
xmasuhai <xmasuhai@163.com>
2026-09-24 21:52:35 +0800

    refactor(build): 移除jsx作用域插件配置
    移除了 @10coding/vite-plugin-jsx-scoped 插件的相关配置，
    包括 vite.config.ts 中的插件导入和配置项，
    以及 tsconfig.app.json 中对应的类型声明。

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import jsxScoped from '@10coding/vite-plugin-jsx-scoped'
+// import jsxScoped from '@10coding/vite-plugin-jsx-scoped'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mangosteen-fe-vue-1-publish/',
   plugins: [
-    jsxScoped({ warnMultiScopedImport: true }),
+    // jsxScoped({ warnMultiScopedImport: true }),
     vue(),
     vueJsx(),
     vueDevTools(),
