2c7ffef5c884ebaf2a2935af1a6c04b8d9f51260
xmasuhai <xmasuhai@163.com>
2026-09-26 14:21:35 +0800

    feat(welcome): 完善欢迎页面UI设计
    - 添加了猪图标资产并集成到第一页
    - 实现了渐变背景和响应式布局
    - 增加了导航链接功能
    - 优化了页面结构和样式设计
    - 集成了山竹记账logo

--- src/modules/welcome/WelCome1stPage.tsx
+++ src/modules/welcome/WelCome1stPage.tsx
@@ -1,14 +1,20 @@
 import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
+import pig from '@/assets/icons/pig.svg'
 
 export const WelCome1stPage = defineComponent({
   setup: (/* props, context */) => {
     return () => (
-      <div
-        class="bg-white h-full mb-62px ml-16px
-        mr-16px border-3px border-solid border-red">
-        <div class="description"></div>
-        <div class="go-next"></div>
-      </div>
+      <>
+        <img src={pig} alt="icon" class="w-128px h-130px mt-25%" />
+        <div class="description flex flex-col items-center text-[2em]">
+          <h2>会挣钱</h2>
+          <h2>还要会省钱</h2>
+        </div>
+        <div class="go-next text-primaryColor mb-84px text-[2em] font-bold">
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+        </div>
+      </>
     )
   },
 })
--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -1,15 +1,22 @@
 import { defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
 import s from './WelcomeView.module.scss'
+import logo from '@/assets/icons/mangosteen.svg'
+import { cn } from 'cn'
 
 export const WelcomeView = defineComponent({
   setup: (/* props, context */) => {
     return () => (
       <div class={s.wrapper}>
         <header class={s.title}>
-          <h1 class="text-blue">山竹记账</h1>
+          <img src={logo} alt="logo" />
+          <h1>山竹记账</h1>
         </header>
-        <main>
+        <main
+          class={cn([
+            'bg-welcomeCardBg mb-62px ml-16px mr-16px rounded-lg',
+            'flex flex-col flex-grow items-center justify-around',
+          ])}>
           <RouterView />
         </main>
       </div>
